b89107b286d4e88b38433714107d76cf8ea76113
VLAD <vtsitsyura@knu.ua>
2023-10-24 20:53:59 +0300

    Last version

--- Pacman/Properties/Resources.Designer.cs
+++ Pacman/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Pacman.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -95,6 +95,16 @@ namespace Pacman.Properties {
         internal static System.Drawing.Bitmap Board_1 {
             get {
                 object obj = ResourceManager.GetObject("Board 1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Board_2 {
+            get {
+                object obj = ResourceManager.GetObject("Board 2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
